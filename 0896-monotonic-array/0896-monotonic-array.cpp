57c0b146a51d09fe10b02870f70578ce96f53edb
Richarani-tarase <145827107+Richarani-tarase@users.noreply.github.com>
2023-09-26 19:22:20 +0530

    Time: 145 ms (7.16%), Space: 96.7 MB (19.85%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -0,0 +1,44 @@
+class Solution {
+    // int lengthasc(vector<int> & nums){
+    //     int count=1;
+    //     for(int i=0;i<nums.size()-1;i++){
+    //         if(nums[i]<=nums[i+1]){
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
+    //  int lengthdec(vector<int> & nums){
+    //     int count=1;
+    //     for(int i=0;i<nums.size()-1;i++){
+    //         if(nums[i]>=nums[i+1]){
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int c=1;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]>=nums[i+1]){
+                c++;
+            }
+        }
+        if(c==nums.size())
+        {
+            return true;
+        }
+        c=1;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]<=nums[i+1]){
+                c++;
+            }
+        }    
+        if(c==nums.size()){
+            return 1;
+        }
+        return 0;
+    
+    }
+};
