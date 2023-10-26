dd4653093fadba9a4f4e873fe271349077fb17f5
Richarani-tarase <145827107+Richarani-tarase@users.noreply.github.com>
2023-10-27 00:33:33 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (46.63%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -1,37 +1,20 @@
-// class Solution {
-// public:
-//     bool isSameAfterReversals(int num) {
-//         int temp=num,rev=0,rem=0;
-//         int a=2;
-//         while(a>0)
-//         {    
-//             while(temp!='\0')
-//             {
-//                 rem=temp%10;
-//                 rev=(rev*10)+rem;
-//                 temp=temp/10;
-//             }
-//             temp=rev;
-//              rev=0,rem=0;
-//             a--;
-//         }   
-//         return (num==temp);
-//     }
-// };
 class Solution {
 public:
-int reverse(int num){
-    int ans=0;
-    while(num!=0){
-        int rem=num%10;
-        ans=(ans*10)+rem;
-        num=num/10;
-    }
-    return ans;
-}
     bool isSameAfterReversals(int num) {
-       int ans=reverse(num);
-       ans=reverse(ans);
-       return (num==ans); 
+        int temp=num;
+        int a=2;
+        while(a>0)
+        {    
+            int rev=0,rem=0;
+            while(temp!='\0')
+            {
+                rem=temp%10;
+                rev=(rev*10)+rem;
+                temp=temp/10;
+            }
+            temp=rev;
+            a--;
+        }   
+        return (num==temp);
     }
 };
