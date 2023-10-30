f5fc10a6db7d7ec3e2f28e9aa7917ac09302634c
Richarani-tarase <145827107+Richarani-tarase@users.noreply.github.com>
2023-10-30 18:56:16 +0530

    Time: 15 ms (92.55%), Space: 71.6 MB (56.12%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -26,41 +26,47 @@ public:
         ListNode* head=reverse(l1);
         ListNode* head2=reverse(l2),*temp,*p=NULL;
         int carry=0,a=0;
-        while(head!=NULL && head2!=NULL)
+        while(head!=NULL || head2!=NULL)
         {
-            temp=new ListNode();
-            a=carry+head->val+head2->val;
-            temp->val=a%10;
-            temp->next=p;
-            p=temp;
-            head=head->next;
-            head2=head2->next;
-            carry=a/10;
-        }
-        if(head!=NULL || head2!=NULL)
-        {
-            while(head!=NULL)
-            {
+            if(head!=NULL && head2!=NULL)
+            {   
                 temp=new ListNode();
-                int a=carry+head->val;
-                carry=a/10;
+                a=carry+head->val+head2->val;
                 temp->val=a%10;
                 temp->next=p;
                 p=temp;
-                head=head->next;  
-            }
-            while(head2!=NULL)
-            {
-                temp=new ListNode();
-                int a=carry+head2->val;
+                head=head->next;
+                head2=head2->next;
                 carry=a/10;
-                temp->val=a%10;
-                temp->next=p;
-                p=temp;
-                head2=head2->next;  
+            }
+            if(head!=NULL && head2==NULL)
+            {
+                while(head!=NULL)
+                {
+                    temp=new ListNode();
+                    int a=carry+head->val;
+                    carry=a/10;
+                    temp->val=a%10;
+                    temp->next=p;
+                    p=temp;
+                    head=head->next;  
+                }
+            }
+            if(head2!=NULL&&head==NULL)
+            {   
+                while(head2!=NULL)
+                {
+                    temp=new ListNode();
+                    int a=carry+head2->val;
+                    carry=a/10;
+                    temp->val=a%10;
+                    temp->next=p;
+                    p=temp;
+                    head2=head2->next;  
+                }
             }
         }
-        if(head==NULL&&head2==NULL&&carry)
+         if(head==NULL&&head2==NULL&&carry)
         {
             temp=new ListNode();
             temp->val=carry;
