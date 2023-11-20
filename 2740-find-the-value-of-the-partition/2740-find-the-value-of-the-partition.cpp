fdeeb54a4e16411537187639885419390c7ea579
Richarani-tarase <145827107+Richarani-tarase@users.noreply.github.com>
2023-11-20 23:13:43 +0530

    Time: 114 ms (6.19%), Space: 59.4 MB (65.07%) - LeetHub

--- 2740-find-the-value-of-the-partition/2740-find-the-value-of-the-partition.cpp
+++ 2740-find-the-value-of-the-partition/2740-find-the-value-of-the-partition.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int findValueOfPartition(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int n=nums.size();
-         int m,ans=nums[n-1];
-        for(int i=0;i<n-1;i++)
+        int n=nums.size()-1;
+         int m,ans=nums[n];
+        for(int i=0;i<n;i++)
         {
             m=nums[i+1]-nums[i];
             ans=min(m,ans);
