5db6997d07ec668fd3b1cdf1e23c0ae13117ddcc
Richarani-tarase <145827107+Richarani-tarase@users.noreply.github.com>
2024-01-27 16:11:02 +0530

    Time: 35 ms (16.16%), Space: 17.8 MB (6.47%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -1,42 +1,24 @@
 class Solution {
 public:
-    // string largestOddNumber(string num)
-    // {
-        // string max;
-        // int c=0;
-        // for(int i=num.size()-1;i>=0;i--)
-        // {
-        //     if(num[i]%2==0)
-        //     {
-        //         c++;
-        //     }
-        //     else
-        //     {
-        //         break;
-        //     }
-        // }
-        // for(int i=0;i<num.size()-c;i++)
-        // {
-        //     max+=num[i];
-        // }
-        // return max;
-        string copy(string num,int n){
-        string ans="";
-        for(int i=0;i<=n;i++){
-            ans+=num[i];
+    string largestOddNumber(string num)
+    {
+        string max;
+        int c=0;
+        for(int i=num.size()-1;i>=0;i--)
+        {
+            if(num[i]%2==0)
+            {
+                c++;
+            }
+            else
+            {
+                break;
+            }
         }
-        return ans;
-    }
-    string largestOddNumber(string num) {
-       string maxi="";
-       int n=num.size();
-       for(int i=num.size();i>=0;i--){
-        char c=num[i];
-        if(c=='1' ||c=='3' ||c=='5' ||c=='7' ||c=='9'){
-            maxi=copy(num,i);
-            break;
+        for(int i=0;i<num.size()-c;i++)
+        {
+            max+=num[i];
         }
-       }
-       return maxi;
+        return max;
     }
 };
