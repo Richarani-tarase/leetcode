4ad800ec6c9955524b37feb441c7c39c466a2feb
Richarani-tarase <145827107+Richarani-tarase@users.noreply.github.com>
2024-01-30 13:15:55 +0530

    Time: 2 ms (43.54%), Space: 9.6 MB (10.11%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
-         vector<string> v;
+        vector<string> v;
         int j=0;
         int m=target.size()-1;
+        // for(int i=1;i<=target[m];i++)
+        // {
+        //     if(target[j]==i)
+        //     {
+        //         v.push_back("Push");
+        //         j++;
+        //     }
+        //     else
+        //     {
+        //         v.push_back("Push");
+        //         v.push_back("Pop");
+        //     }
+        // }
+        stack<int>s;
         for(int i=1;i<=target[m];i++)
         {
-            if(target[j]==i)
+            s.push(i);
+            if(s.top()==target[j])
             {
                 v.push_back("Push");
                 j++;
@@ -14,6 +29,7 @@ public:
             else
             {
                 v.push_back("Push");
+                s.pop();
                 v.push_back("Pop");
             }
         }
