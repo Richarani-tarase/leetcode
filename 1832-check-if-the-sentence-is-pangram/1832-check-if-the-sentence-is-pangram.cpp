6bb96f095c1cc2a6ea1af0c587553686ed45654d
Richarani-tarase <145827107+Richarani-tarase@users.noreply.github.com>
2024-03-30 09:59:08 +0530

    Time: 6 ms (7.16%), Space: 9 MB (13.39%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        vector<int> ans(26,0);
-       int a;
-       int count=0;
-       for(int i=0;i<sentence.size();i++){
-           a=sentence[i]-'a';
-           ans[a]=1;
-       }
-       for(int i=0;i<26;i++){
-           if(ans[i]!=1){
-               return 0;
-           }
-       }
-       return 1;
+        map<char,int> m;
+        for(auto x:sentence)
+        {
+            m[x]++;
+        }
+        if(m.size()==26)
+        {
+            return 1;
+        }
+        return 0;
     }
 };
