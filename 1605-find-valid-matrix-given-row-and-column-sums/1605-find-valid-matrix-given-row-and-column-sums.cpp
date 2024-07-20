6a86ec45e526029034ccf6e8046ac5441cdb87c3
Richarani-tarase <145827107+Richarani-tarase@users.noreply.github.com>
2024-07-20 22:28:33 +0530

    Time: 43 ms (23.25%), Space: 68.1 MB (5.19%) - LeetHub

---
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int find_min(vector<int> v)
+    {
+        int n=v.size();
+        int min=-1;
+        int minele=INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            if(v[i]>-1 && v[i]<minele)
+            {
+                minele=v[i];
+                min=i;
+            }
+        }
+        return min;
+    }
+    bool check(const vector<int>& ve) {
+        for (int val : ve) {
+            if (val > -1) {
+                return true;
+            }
+        }
+        return false;
+    }
+    vector<vector<int>> restoreMatrix(vector<int>& r, vector<int>& c) {
+        int n=r.size();
+        int m=c.size();
+        vector<vector<int>>v(n,vector<int>(m,0));
+        while(check(r))
+        {
+            int a=find_min(r);
+            int b=find_min(c);
+            if (a == -1 || b == -1) {
+                break;
+            }
+            if(r[a]<c[b])
+            {
+                v[a][b]=r[a];
+                c[b]-=r[a];
+                r[a]=-1;
+            }
+            else
+            {
+                v[a][b]=c[b];
+                r[a]-=c[b];
+                c[b]=-1;
+            }
+        }
+          return v;  
+    }
+};
