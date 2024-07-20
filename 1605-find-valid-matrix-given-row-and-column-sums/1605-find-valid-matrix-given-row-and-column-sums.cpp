543ba1289703f0b4d09988ec7935dfbfa97c849a
Richarani-tarase <145827107+Richarani-tarase@users.noreply.github.com>
2024-07-20 22:30:56 +0530

    Time: 36 ms (67.27%), Space: 36 MB (16.25%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
@@ -1,50 +1,59 @@
 class Solution {
 public:
-    int find_min(vector<int> v)
-    {
-        int n=v.size();
-        int min=-1;
-        int minele=INT_MAX;
-        for(int i=0;i<n;i++)
-        {
-            if(v[i]>-1 && v[i]<minele)
-            {
-                minele=v[i];
-                min=i;
-            }
-        }
-        return min;
-    }
-    bool check(const vector<int>& ve) {
-        for (int val : ve) {
-            if (val > -1) {
-                return true;
-            }
-        }
-        return false;
-    }
+    // int find_min(vector<int> v)
+    // {
+    //     int n=v.size();
+    //     int min=-1;
+    //     int minele=INT_MAX;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(v[i]>-1 && v[i]<minele)
+    //         {
+    //             minele=v[i];
+    //             min=i;
+    //         }
+    //     }
+    //     return min;
+    // }
+    // bool check(const vector<int>& ve) {
+    //     for (int val : ve) {
+    //         if (val > -1) {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
     vector<vector<int>> restoreMatrix(vector<int>& r, vector<int>& c) {
         int n=r.size();
         int m=c.size();
         vector<vector<int>>v(n,vector<int>(m,0));
-        while(check(r))
+        // while(check(r))
+        // {
+        //     int a=find_min(r);
+        //     int b=find_min(c);
+        //     if (a == -1 || b == -1) {
+        //         break;
+        //     }
+        //     if(r[a]<c[b])
+        //     {
+        //         v[a][b]=r[a];
+        //         c[b]-=r[a];
+        //         r[a]=-1;
+        //     }
+        //     else
+        //     {
+        //         v[a][b]=c[b];
+        //         r[a]-=c[b];
+        //         c[b]=-1;
+        //     }
+        // }
+        for(int i=0;i<n;i++)
         {
-            int a=find_min(r);
-            int b=find_min(c);
-            if (a == -1 || b == -1) {
-                break;
-            }
-            if(r[a]<c[b])
+            for(int j=0;j<m;j++)
             {
-                v[a][b]=r[a];
-                c[b]-=r[a];
-                r[a]=-1;
-            }
-            else
-            {
-                v[a][b]=c[b];
-                r[a]-=c[b];
-                c[b]=-1;
+                v[i][j]=min(r[i],c[j]);
+                r[i]-=v[i][j];
+                c[j]-=v[i][j];
             }
         }
           return v;  
