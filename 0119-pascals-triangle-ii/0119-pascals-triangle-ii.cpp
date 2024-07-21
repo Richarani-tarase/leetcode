8ff812dc27565c1f429e0f94c0e4c067c68c7be5
Richarani-tarase <145827107+Richarani-tarase@users.noreply.github.com>
2024-07-21 16:59:26 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (17.96%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -3,9 +3,8 @@ public:
     vector<int> getRow(int rowIndex) {
         vector<vector<int>> v;
         v.push_back({1});
-        
         for (int i = 1; i <= rowIndex; ++i) {
-            vector<int> a(i + 1, 1); // Initialize the row with 1s
+            vector<int> a(i + 1, 1);
             for (int j = 1; j < i; ++j) {
                 a[j] = v[i - 1][j - 1] + v[i - 1][j];
             }
