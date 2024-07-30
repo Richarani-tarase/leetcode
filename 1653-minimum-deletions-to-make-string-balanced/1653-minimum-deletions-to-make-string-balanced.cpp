42e1f5b68d3bbb4891cd39acd34b2f09ad8bf07e
Richarani-tarase <145827107+Richarani-tarase@users.noreply.github.com>
2024-07-30 23:23:11 +0530

    Time: 69 ms (96.49%), Space: 24.6 MB (51.83%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+      int n=s.size();
+      int ans=n;
+      int ps[2][n+1];
+      ps[0][0]=0;
+      ps[1][0]=0;
+      for(int i=0;i<n;i++){
+        ps[0][i+1]=ps[0][i]+(s[i]=='a');
+        ps[1][i+1]=ps[1][i]+(s[i]=='b');
+      }
+      for(int i=0;i<=n;i++){
+        int left=ps[1][i];
+        int right=ps[0][n]-ps[0][i];
+        ans=min(ans,left+right);
+      }
+      return ans;
+    }
+};
