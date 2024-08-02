6c9c0f7e1084e1e393cc6db95e64a8ab34688fd3
Richarani-tarase <145827107+Richarani-tarase@users.noreply.github.com>
2024-08-03 00:14:15 +0530

    Time: 38 ms (12.59%), Space: 77.8 MB (23.68%) - LeetHub

--- 2660-determine-the-winner-of-a-bowling-game/2660-determine-the-winner-of-a-bowling-game.cpp
+++ 2660-determine-the-winner-of-a-bowling-game/2660-determine-the-winner-of-a-bowling-game.cpp
@@ -2,25 +2,12 @@ class Solution {
 public:
     int check(vector<int> a)
     {
-        if(a.size()==1)
-        {
-            return a[0];
-        }
+        if(a.size()==1)return a[0];
         int ans=a[0];
-        if(a[0]==10)
+        for(int i=1;i<a.size();i++)
         {
-            ans+=a[1]*2;
-        }
-        else
-        {
-            ans+=a[1];
-        }
-        for(int i=2;i<a.size();i++)
-        {
-            if(a[i-1]==10||a[i-2]==10)
-            {
+            if(a[i-1]==10||(i>1&&a[i-2]==10))
                 ans=ans+(a[i]*2);
-            }
             else
                 ans+=a[i];
         }
@@ -29,14 +16,8 @@ public:
     int isWinner(vector<int>& p1, vector<int>& p2) {
         int a=check(p1);
         int b=check(p2);
-        if(a==b)
-        {
-            return 0;
-        }
-        else if(a<b)
-        {
-            return 2;
-        }
+        if(a==b)return 0;
+        else if(a<b)return 2;
         return 1;
     }
 };
