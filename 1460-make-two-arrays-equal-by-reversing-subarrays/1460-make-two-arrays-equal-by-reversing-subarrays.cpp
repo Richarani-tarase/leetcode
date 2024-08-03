1bd23cab3e63d887ca90f2ac08ac8c3cae7be18d
Richarani-tarase <145827107+Richarani-tarase@users.noreply.github.com>
2024-08-03 10:54:29 +0530

    Time: 9 ms (63.29%), Space: 17.5 MB (42.71%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,33 +1,8 @@
 class Solution {
 public:
-    void rev(vector<int>& arr,int i,int j)
-    {
-        while(i<=j)
-        {
-            swap(arr[i],arr[j]);
-            i++,j--;
-        }
-    }
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        int n=target.size(),m=arr.size();
-        if(n==m)
-        {
-            for(int i=0;i<n;i++)
-            {
-                for(int j=i;j<m;j++)
-                {
-                    if(arr[j]==target[i])
-                    {
-                        rev(arr,i,j);
-                        break;
-                    }
-                }
-            }
-        }
-        if(target==arr)
-        {
-            return true;
-        }
-        return false;
+        sort(target.begin(),target.end());
+        sort(arr.begin(),arr.end());
+        return target==arr;
     }
 };
