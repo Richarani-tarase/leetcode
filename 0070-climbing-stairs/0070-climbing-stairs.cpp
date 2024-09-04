f453e2a06db5e17b1baa39b4ec38a13f0ea04848
Richarani-tarase <145827107+Richarani-tarase@users.noreply.github.com>
2024-09-04 18:31:46 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (35.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        int pre=0,curr=1;
-        for(int i=0;i<n;i++)
+    int topdown(int n,vector<int>& dp)
+    {
+        if(n<=1)
         {
-            int a=curr+pre;
-            pre=curr,curr=a;
+            return n;
         }
-        return curr;
+        if(dp[n]!=-1)
+            return dp[n];
+        return dp[n]=topdown(n-1,dp)+topdown(n-2,dp);
+    }
+    int climbStairs(int n) {
+        // int pre=0,curr=1;
+        // for(int i=0;i<n;i++)
+        // {
+        //     int a=curr+pre;
+        //     pre=curr,curr=a;
+        // }
+        // return curr;
+        vector<int>dp(n+2,-1);
+        return topdown(n+1,dp);
     }
 };
